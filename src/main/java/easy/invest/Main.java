2604931f2804ebad979ef337217267d6a56dfa36
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-08 10:34:34 +0300

    New Main file

---
+++ src/main/java/easy/invest/Main.java
@@ -0,0 +1,29 @@
+package easy.invest;
+
+import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+//import easy.invest.sre.services.FileSystemService;
+//import easy.invest.sre.services.UserService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    private void initDirectory() {
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
