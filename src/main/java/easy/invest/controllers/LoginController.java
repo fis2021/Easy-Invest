5e64089ba57c63bfb74747e1920a1a66215f0ac2
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-12 19:02:01 +0300

    added attributes

--- src/main/java/easy/invest/controllers/LoginController.java
+++ src/main/java/easy/invest/controllers/LoginController.java
@@ -1,5 +1,18 @@
 package easy.invest.controllers;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+public class LoginController {
+    public static Stage stg;
+    @FXML
+    private Text loginMessage;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
 }
+
