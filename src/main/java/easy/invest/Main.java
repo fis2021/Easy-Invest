70da11a3431003e99499e30b4dd850147f34cc9c
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-12 17:44:27 +0300

    new branch

--- src/main/java/easy/invest/Main.java
+++ src/main/java/easy/invest/Main.java
@@ -1,15 +1,11 @@
 package easy.invest;
-
 import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+//import javafx.fxml.FXMLLoader;
 //import easy.invest.sre.services.FileSystemService;
 //import easy.invest.sre.services.UserService;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class Main extends Application {
 
