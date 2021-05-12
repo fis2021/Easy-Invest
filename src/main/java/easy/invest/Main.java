1a0fd9edcaf681572eba784b0d0a2c6a34c104f3
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-12 06:21:50 +0300

    updated Main from example

--- src/main/java/easy/invest/Main.java
+++ src/main/java/easy/invest/Main.java
@@ -1,12 +1,12 @@
 package easy.invest;
 
 import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//import easy.invest.sre.services.FileSystemService;
-//import easy.invest.sre.services.UserService;
+import easy.invest.FileSystemService;
+import easy.invest.services.UserService;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,11 +15,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        initDirectory();
+        UserService.initDatabase();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        primaryStage.setTitle("Registration Example");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
     private void initDirectory() {
-
+        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
+        if (!Files.exists(applicationHomePath))
+            applicationHomePath.toFile().mkdirs();
     }
 
 
