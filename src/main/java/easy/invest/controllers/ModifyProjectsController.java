4463911c1d71844bc2a1fed77db1e0e4156f9948
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-13 17:22:33 +0300

    Implemented subtask EI-30: Create a pop-up window to display the chosen option result.

--- src/main/java/easy/invest/controllers/ModifyProjectsController.java
+++ src/main/java/easy/invest/controllers/ModifyProjectsController.java
@@ -6,6 +6,27 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ModifyProjectsController {
+    public void goToCreate() throws Exception {
+        Parent createWindow = FXMLLoader.load(getClass().getResource("/create_project.fxml"));
+        Scene createScene = new Scene(createWindow);
+        Stage window = new Stage();
+        window.setScene(createScene);
+        window.show();
+    }
 
+    public void goToEdit() throws Exception {
+        Parent editWindow = FXMLLoader.load(getClass().getResource("/edit_project.fxml"));
+        Scene editScene = new Scene(editWindow);
+        Stage window = new Stage();
+        window.setScene(editScene);
+        window.show();
+    }
 
+    public void goToDelete() throws Exception {
+        Parent deleteWindow = FXMLLoader.load(getClass().getResource("/delete_project.fxml"));
+        Scene deleteScene = new Scene(deleteWindow);
+        Stage window = new Stage();
+        window.setScene(deleteScene);
+        window.show();
+    }
 }
