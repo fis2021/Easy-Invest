70e92e04f82a53040f588b6e3ce7168536b25254
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-13 17:05:41 +0300

    Created fxml file and controller

---
+++ src/main/java/easy/invest/controllers/ModifyProjectsController.java
@@ -0,0 +1,11 @@
+package easy.invest.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ModifyProjectsController {
+
+
+}
