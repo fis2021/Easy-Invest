6c448bc859f361ad33f028a69b447f3df746247c
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-13 23:43:12 +0300

    Created method in CreateProjectController for adding project or displaying an error if one or more of the fields are empty

--- src/main/java/easy/invest/controllers/CreateProjectController.java
+++ src/main/java/easy/invest/controllers/CreateProjectController.java
@@ -1,4 +1,42 @@
 package easy.invest.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CreateProjectController {
+    @FXML
+    private TextField titleField;
+    @FXML
+    private TextField descriptionField;
+    @FXML
+    private TextField locationField;
+    @FXML
+    private TextField fundsField;
+    @FXML
+    private Text errorMessage;
+    public void create() {
+        if (titleField.getText() == null || descriptionField.getText() == null || locationField.getText() == null || fundsField.getText() == null) {
+            errorMessage.setVisible(true);
+        } else {
+            try {
+                Scanner username = null;
+                try {
+                    username = new Scanner(new File("log.txt"));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                ProjectService.addProject(titleField.getText(), descriptionField.getText(), (String) locationField.getText(), fundsField.getText(),username);
+            } catch (Exception e) {
+                errorMessage.setText(e.getMessage());
+                errorMessage.setVisible(true);
+            }
+        }
+    }
+
+
 }
