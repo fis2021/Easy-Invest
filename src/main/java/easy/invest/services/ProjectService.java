65adb012b3b169feee3c79a6cb91825435178e56
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-14 03:43:11 +0300

    Implemented functionality add projects to a new database

--- src/main/java/easy/invest/services/ProjectService.java
+++ src/main/java/easy/invest/services/ProjectService.java
@@ -1,5 +1,35 @@
 package easy.invest.services;
 
+import easy.invest.exceptions.TitleAlreadyExists;
+import easy.invest.model.Project;
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+
+import java.util.Objects;
+
+import static easy.invest.services.FileSystemService.getPathToFile;
 public class ProjectService {
+
+    private static ObjectRepository<Project> projectRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("easy-invest2.db").toFile())
+                .openOrCreate("test", "test");
+
+        projectRepository = database.getRepository(Project.class);
+    }
+
+    public static void addProject(String title, String description, String location, String funds, String username) throws TitleAlreadyExists {
+        checkProjectDoesNotAlreadyExist(title);
+        projectRepository.insert(new Project(title,description,location,funds,username));
+    }
+
+    private static void checkProjectDoesNotAlreadyExist(String title) throws TitleAlreadyExists {
+        for (Project project : projectRepository.find()) {
+            if (Objects.equals(title, project.getTitle()))
+                throw new TitleAlreadyExists(title);
+        }
+    }
 
 }
