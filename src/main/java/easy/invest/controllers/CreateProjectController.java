4d3872610c7c4c82543aee3f6f3e8021dd8ad05e
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-14 05:05:03 +0300

    Solved some errors

--- src/main/java/easy/invest/controllers/CreateProjectController.java
+++ src/main/java/easy/invest/controllers/CreateProjectController.java
@@ -2,11 +2,16 @@ package easy.invest.controllers;
 
 import easy.invest.services.ProjectService;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class CreateProjectController {
@@ -20,7 +25,8 @@ public class CreateProjectController {
     private TextField fundsField;
     @FXML
     private Text errorMessage;
-    public void create() {
+
+    public void create() throws IOException {
         if (titleField.getText() == null || descriptionField.getText() == null || locationField.getText() == null || fundsField.getText() == null) {
             errorMessage.setVisible(true);
         } else {
@@ -34,12 +40,28 @@ public class CreateProjectController {
                     e.printStackTrace();
                 }
                 ProjectService.addProject(titleField.getText(), descriptionField.getText(), (String) locationField.getText(), fundsField.getText(),username);
+                Parent modifyWindow = FXMLLoader.load(getClass().getResource("/modify_projects.fxml"));
+                Scene modifyScene = new Scene(modifyWindow);
+                Stage window = new Stage();
+                window.setScene(modifyScene);
+                window.show();
+                Stage stage = (Stage) errorMessage.getScene().getWindow();
+                stage.close();
             } catch (Exception e) {
-                errorMessage.setText(e.getMessage());
                 errorMessage.setVisible(true);
+                errorMessage.setText(e.getMessage());
             }
         }
     }
 
 
+    public void cancel() throws IOException {
+        Parent modifyWindow = FXMLLoader.load(getClass().getResource("/modify_projects.fxml"));
+        Scene modifyScene = new Scene(modifyWindow);
+        Stage window = new Stage();
+        window.setScene(modifyScene);
+        window.show();
+        Stage stage = (Stage) errorMessage.getScene().getWindow();
+        stage.close();
+    }
 }
