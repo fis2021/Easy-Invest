d837839df2d3ddd8e98502bc1e2b9f899b0414d9
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-16 22:22:58 +0300

    Added @FXML to controller methods

--- src/main/java/easy/invest/controllers/ModifyProjectsController.java
+++ src/main/java/easy/invest/controllers/ModifyProjectsController.java
@@ -1,11 +1,13 @@
 package easy.invest.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ModifyProjectsController {
+    @FXML
     public void goToCreate() throws Exception {
         Parent createWindow = FXMLLoader.load(getClass().getResource("/create_project.fxml"));
         Scene createScene = new Scene(createWindow);
@@ -13,7 +15,7 @@ public class ModifyProjectsController {
         window.setScene(createScene);
         window.show();
     }
-
+    @FXML
     public void goToEdit() throws Exception {
         Parent editWindow = FXMLLoader.load(getClass().getResource("/edit_project.fxml"));
         Scene editScene = new Scene(editWindow);
@@ -21,7 +23,7 @@ public class ModifyProjectsController {
         window.setScene(editScene);
         window.show();
     }
-
+    @FXML
     public void goToDelete() throws Exception {
         Parent deleteWindow = FXMLLoader.load(getClass().getResource("/delete_project.fxml"));
         Scene deleteScene = new Scene(deleteWindow);
