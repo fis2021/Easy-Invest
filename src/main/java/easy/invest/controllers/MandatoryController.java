d087fb76abb0211cc5deab64ff19270d5b5326d2
LaviniaRus <lavinia.rus20@gmail.com>
2021-05-16 12:01:53 +0300

    Created ok method

--- src/main/java/easy/invest/controllers/MandatoryController.java
+++ src/main/java/easy/invest/controllers/MandatoryController.java
@@ -1,6 +1,15 @@
 package easy.invest.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class MandatoryController {
+    @FXML
+    private Text title;
+    @FXML
     public void ok() {
+        Stage stage = (Stage) title.getScene().getWindow();
+        stage.close();
     }
 }
